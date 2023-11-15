7e9b5fafaf2b54aeff146df6caa9e39b7ed34c86
NicolasTassin <54062897+NicolasTassin@users.noreply.github.com>
2023-11-15 09:18:19 +0100

    Cleaning Map

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import React, { useRef, useEffect, useState, useCallback } from "react";
 import "./App.css";
 import Map from "./components/Map";
 import PlotComponent from "./components/Plot";
-import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
-import Plotly from "plotly.js-dist";
-const plots = require("./test.json");
+
 
 function App() {
   const [selectedFlare, setSelectedFlare] = useState(
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useRef } from "react";
 import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
 
-mapboxgl.accessToken =
-  "pk.eyJ1Ijoia3Jpc3RpbmFtYXJ0aW5rIiwiYSI6ImNsb3U3MTVmazBobm4yanFkOW1jY2R2c24ifQ.KxLIQK6WRueHi9oTzI54ig";
+mapboxgl.accessToken = ""//<API_KEY>
+  
 
 const flares = [
   {
@@ -25,7 +25,7 @@ const flares = [
 const Map = ({ setSelectedFlare }) => {
   const mapContainer = useRef(null);
   const map = useRef(null);
-    console.log(process.env.local.MAPBOX_API_KEY, "process .env")
+    
   useEffect(() => {
     if (!map.current) {
       map.current = new mapboxgl.Map({
@@ -49,7 +49,7 @@ const Map = ({ setSelectedFlare }) => {
         });
       });
     }
-  }, [flares]);
+  }, [setSelectedFlare]);
 
   return <div ref={mapContainer} className="container" />;
 };
