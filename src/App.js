c12fdc66f08d3c980f76b52b4fd23b7f0cf5d156
NicolasTassin <54062897+NicolasTassin@users.noreply.github.com>
2023-11-15 07:32:12 +0100

    Plot to component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { useRef, useEffect, useState, useCallback } from "react";
 import "./App.css";
-import Map from './components/Map'
+import Map from "./components/Map";
+import PlotComponent from "./components/Plot";
 import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
-import Plotly from "plotly.js-dist"
+import Plotly from "plotly.js-dist";
 const plots = require("./test.json");
 
 mapboxgl.accessToken =
   "pk.eyJ1Ijoia3Jpc3RpbmFtYXJ0aW5rIiwiYSI6ImNsb3U3MTVmazBobm4yanFkOW1jY2R2c24ifQ.KxLIQK6WRueHi9oTzI54ig";
-
-
 
 function App() {
   const mapContainer = useRef(null);
@@ -19,11 +18,11 @@ function App() {
   const [selectedFlare, setSelectedFlare] = useState(
     "Click on the map to select a flare to analyse"
   );
-  const [dataSelected, setDataSelected] = useState();
+  
 
-  console.log(selectedFlare, "selected flare in App")
+  console.log(selectedFlare, "selected flare in App");
   // const createMapAndMarkers = useCallback(() => {
-  //   if (map.current) return; 
+  //   if (map.current) return;
   //   map.current = new mapboxgl.Map({
   //     container: mapContainer.current,
   //     style: "mapbox://styles/mapbox/streets-v12",
@@ -44,108 +43,55 @@ function App() {
   //     });
   //   });
   // }, [lng, lat, zoom]);
-  
-    
-    const createPlot = () => {
-      let plotX = [];
-    let plotY = [];
-    
-      const filteredFlareData = plots
-      .filter((plot) => plot.flare_stack_name === selectedFlare)
-      .forEach((value, index) => {
-        const parsedValue = Number(value[dataSelected]);
-        const parsedTime = Number(value.time)
-        if (!isNaN(parsedValue)) {
-          plotY.push(parsedValue);
-          plotX.push(parsedTime);
-        }
-      });
 
-      let data = {
-        type: 'scatter',
-        x: plotX,
-        y: plotY,
-        mode: 'lines',
-        name: 'Red',
-        line: {
-          color: 'rgb(219, 64, 82)',
-          width: 3
-        }
-      };
+  // const createPlot = () => {
+  //   let plotX = [];
+  //   let plotY = [];
 
-      let layout = {
-        width: 500,
-        height: 500
-      };
+  //   const filteredFlareData = plots
+  //     .filter((plot) => plot.flare_stack_name === selectedFlare)
+  //     .forEach((value, index) => {
+  //       const parsedValue = Number(value[dataSelected]);
+  //       const parsedTime = Number(value.time);
+  //       if (!isNaN(parsedValue)) {
+  //         plotY.push(parsedValue);
+  //         plotX.push(parsedTime);
+  //       }
+  //     });
 
-      Plotly.newPlot('plot', [data], layout);
+  //   let data = {
+  //     type: "scatter",
+  //     x: plotX,
+  //     y: plotY,
+  //     mode: "lines",
+  //     name: "Red",
+  //     line: {
+  //       color: "rgb(219, 64, 82)",
+  //       width: 3,
+  //     },
+  //   };
 
-      
-      
-      console.log(plotX, "plotX");
-    console.log(plotY, "plotY");
-    }
-    
+  //   let layout = {
+  //     width: 500,
+  //     height: 500,
+  //   };
 
-  useEffect(() => {
-    //createMapAndMarkers();
-    createPlot()
-    console.log("inside useEffect")
-    
-    
-  }, [ createPlot, dataSelected, selectedFlare]);
+  //   Plotly.newPlot("plot", [data], layout);
+
+  //   console.log(plotX, "plotX");
+  //   console.log(plotY, "plotY");
+  // };
+
+  // useEffect(() => {
+  //   //createMapAndMarkers();
+  //   createPlot();
+  //   console.log("inside useEffect");
+  // }, [createPlot, dataSelected, selectedFlare]);
 
   return (
     <div className="main">
-      <Map setSelectedFlare={setSelectedFlare}/>
-      <div id='plot' className="container plot">
-        {/* <Plot
-          data={[
-            {
-              x: plotX,
-              y: plotY,
-              type: "scatter",
-              mode: "lines",
-              marker: { color: "red" },
-            },
-          ]}
-          layout={{ width: 640, height: 480, title: selectedFlare, datarevision: 'true' }}
-        /> */}
-        <div className="">
-          <p>Please select data to analyse</p>
-          <p>{dataSelected}</p> {" "}
-          <input
-            type="radio"
-            id="co2_rate"
-            name="data"
-            value="co2_rate"
-            onClick={(e) => {
-              setDataSelected(e.target.value);
-            }}
-          />
-            <label for="co2_rate">CO2 Rate</label> {" "}
-          <input
-            type="radio"
-            id="volume_tracked"
-            name="data"
-            value="volume_tracked"
-            onClick={(e) => {
-              setDataSelected(e.target.value);
-            }}
-          />
-            <label for="volume_tracked">Volume tracked</label> {" "}
-          <input
-            type="radio"
-            id="rate_estimated"
-            name="data"
-            value="rate_estimated"
-            onClick={(e) => {
-              setDataSelected(e.target.value);
-            }}
-          />
-            <label for="rate_estimated">Rate estimated</label>
-        </div>
-      </div>
+      <Map setSelectedFlare={setSelectedFlare} />
+      <PlotComponent selectedFlare={selectedFlare}/>
     </div>
   );
 }
